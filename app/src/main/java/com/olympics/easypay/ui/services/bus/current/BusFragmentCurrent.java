6724d8ff62628cabfc4c89a8532eaa020724ca90
Ahmed3mad <3umduck@gmail.com>
2020-02-19 00:57:24 +0200

    check valid email

--- app/src/main/java/com/olympics/easypay/ui/services/bus/current/BusFragmentCurrent.java
+++ app/src/main/java/com/olympics/easypay/ui/services/bus/current/BusFragmentCurrent.java
@@ -11,6 +11,11 @@ import androidx.fragment.app.Fragment;
 
 import com.olympics.easypay.R;
 
+import java.util.Calendar;
+
+import static com.olympics.easypay.utils.Constants.BUS_TIME;
+import static com.olympics.easypay.utils.Constants.SHARED_PREFS;
+
 public class BusFragmentCurrent extends Fragment {
 
     private static final String TAG = "MyTag";
@@ -29,7 +34,12 @@ public class BusFragmentCurrent extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        loadFragment(new BusFragmentCurrentSuccess());
+        long l = getActivity().getSharedPreferences(SHARED_PREFS, 0).getLong(BUS_TIME, 0);
+        if (Calendar.getInstance().getTimeInMillis() - l < 7200000) {
+            loadFragment(new BusFragmentCurrentPending());
+        } else {
+            loadFragment(new BusFragmentCurrentSuccess());
+        }
     }
 
     private void loadFragment(Fragment fragment) {
